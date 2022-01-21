50d7b4f0a497dad3a3bbdad7a27fd09716369e8e
helloftroy <83493935+helloftroy@users.noreply.github.com>
2022-01-21 12:17:17 +0000

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -200,9 +200,12 @@ elif choice == 'MAP':
     #draw_kegg_map(pathinput)
 
     st.write("### KEGG Pathway with Halomonas TD1.0 genes highlighted")
-    with open('../omics_td1.0/outputdata/ko00020.pdf',"rb") as f:
+    with open('../outputdata/ko00020.pdf',"rb") as f:
          base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-         pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="900" height="700" type="application/pdf"></iframe>'
+         pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+
+         
+        #pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="900" height="700" type="application/pdf"></iframe>'
          st.markdown(pdf_display, unsafe_allow_html=True)
     st.write("### List of all genes in pathway chosen!")
     #st.table(str)
