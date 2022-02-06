adb477658e8716ad0b6af2d16436ccc31120473d
helloftroy <83493935+helloftroy@users.noreply.github.com>
2022-02-06 18:44:26 +0000

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -205,7 +205,7 @@ elif choice == 'MAP':
     st.write("### KEGG Pathway with Halomonas TD1.0 genes highlighted")
     
     with open(os.path.join(outdir, img_filename),"rb") as f:
-        base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
         pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
         st.markdown(pdf_display, unsafe_allow_html=True)
         
