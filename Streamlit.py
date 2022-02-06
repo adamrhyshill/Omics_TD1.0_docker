ccb7af7c4b1ae60c2ff36a50de87f8be67e02ddf
helloftroy <83493935+helloftroy@users.noreply.github.com>
2022-02-06 18:54:58 +0000

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -189,8 +189,10 @@ elif choice == 'MAP':
     pathway = KGML_parser.read(kegg_get(pathinput, "kgml"))
     canvas = KGMLCanvas(pathway, import_imagemap=True)
     
-    outdir = os.path.split('https://github.com/helloftroy/Omics_TD1.0/blob/main/')
-    st.write(outdir)
+    #outdir = os.path.split('https://github.com/helloftroy/Omics_TD1.0/blob/main/')
+    outdir = 'https://github.com/helloftroy/Omics_TD1.0/blob/main/'
+
+    
     #'../omics_td1.0/outputdata/'
     canvas.draw(os.path.join(outdir, img_filename))
     
